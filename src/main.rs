7e6bf71e4e679002e2463a821868d11c3ce8e50e
Joao Meyer <1994meyer@gmail.com>
2022-01-04 23:45:32 +0000

    Implemented tests and base functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::fs;
+
 mod parser;
 
 fn main() {
-    let subs = parser::from_file("sintel_en.srt");
+    let data = fs::read_to_string("sintel_en.srt").unwrap();
+    let subs = parser::from_str(&data);
     println!("{:?}", subs);
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,8 @@
-use std::fs;
 use std::time::Duration;
 
 #[allow(unused_imports)]
 use pest::Parser;
+use pest::iterators::Pair;
 use pest_derive::*;
 
 
@@ -11,7 +11,7 @@ use pest_derive::*;
 struct SrtParser;
 
 
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Default)]
 pub struct Sub {
     pub id: usize,
     pub start: Duration,
@@ -20,38 +20,37 @@ pub struct Sub {
 }
 
 
-pub fn from_file(filename: &str) -> Vec<Sub> {
-    let data = fs::read_to_string(filename).unwrap();
-
-    let items = SrtParser::parse(Rule::subs, &data)
-        .expect("Error while parsing subs")
-        .next()
-        .expect("Error iterating over result");
-
-    let mut subs = Vec::new();
-
-    for sub in items.into_inner() {
+impl Sub {
+    fn from_pair(pair: Pair<Rule>) -> Sub {
         let mut default = Sub::default();
 
-        for part in sub.into_inner() {
-            match part.as_rule() {
-                Rule::id => {
-                    default.id = part.as_str().parse::<usize>().unwrap();
-                }
-                Rule::start => {
-                    default.start = parse_duration(part.as_str());
-                }
-                Rule::end => {
-                    default.end = parse_duration(part.as_str());
-                }
-                Rule::text => {
-                    default.text = part.as_str().to_string()
-                }
+        for item in pair.into_inner() {
+            match item.as_rule() {
+                Rule::id    => { default.id = item.as_str().parse::<usize>().unwrap(); }
+                Rule::start => { default.start = parse_duration(item.as_str()); }
+                Rule::end   => { default.end = parse_duration(item.as_str()); }
+                Rule::text  => { default.text = item.as_str().to_string() }
                 _ => unreachable!()
             }
         }
 
-        subs.push(default);
+        return default;
+    }
+}
+
+
+pub fn from_str(data: &str) -> Vec<Sub> {
+    let items = SrtParser::parse(Rule::subs, &data)
+        .expect("Error while parsing string");
+
+    let mut subs = Vec::new();
+
+    // This loop should only run once
+    for item in items {
+        for sub in item.into_inner() {
+            let sub = Sub::from_pair(sub);
+            subs.push(sub);
+        }
     }
 
     return subs;
@@ -94,5 +93,44 @@ mod tests {
         expected += Duration::from_secs(32);
         expected += Duration::from_millis(555);
         assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_from_pair() {
+        let data = concat!(
+            "1\n",
+            "00:00:01,250 --> 00:00:02,500\n",
+            "This blade has a dark past.\n\n"
+        );
+
+        let parsed = SrtParser::parse(Rule::srt, &data)
+            .unwrap()
+            .next()
+            .unwrap();
+
+        let sub = Sub::from_pair(parsed);
+
+        assert_eq!(sub.id, 1);
+        assert_eq!(sub.start, Duration::new(1, 250_000_000));
+        assert_eq!(sub.end, Duration::new(2, 500_000_000));
+        assert_eq!(sub.text, "This blade has a dark past.");
+    }
+
+    #[test]
+    fn test_from_str() {
+        let data = concat!(
+            "1\n",
+            "00:00:01,250 --> 00:00:02,500\n",
+            "This blade has a dark past.\n\n"
+        );
+
+        let mut subs = from_str(data);
+        assert_eq!(subs.len(), 1);
+
+        let sub = subs.pop().unwrap();
+        assert_eq!(sub.id, 1);
+        assert_eq!(sub.start, Duration::new(1, 250_000_000));
+        assert_eq!(sub.end, Duration::new(2, 500_000_000));
+        assert_eq!(sub.text, "This blade has a dark past.");
     }
 }
